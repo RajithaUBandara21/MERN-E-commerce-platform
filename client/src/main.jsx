4da7e62ed51704faf5fe2bd8da8328eb26bbd9d5
NelunikaDGNN <nelunikanuwanthi@gmail.com>
2024-12-09 23:26:03 +0530

    Updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Provider store={store}>
-
-  <App />
-
-  </Provider>
-  
-  </BrowserRouter>
- 
-)
-=======
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -35,4 +15,4 @@ createRoot(document.getElementById("root")).render(
     </Provider>
   </BrowserRouter>
 );
->>>>>>> 7e1abda (Add to cart function created)
+
