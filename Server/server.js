07c8160a8b18d80de45d051b66764d2fffe91172
Rajitha Bandara <138967884+RajithaUBandara21@users.noreply.github.com>
2024-12-10 20:17:32 +0530

    cors problem fixed docarization done

--- Server/server.js
+++ Server/server.js
@@ -33,11 +33,11 @@ mongoose
     process.exit(1); // Exit the process with a failure code
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
