94ee88e13d35615396c9344af0f77c4550be1a6c
Rajitha Bandara <138967884+RajithaUBandara21@users.noreply.github.com>
2024-12-10 11:09:49 +0530

    conflict fixed

--- Server/server.js
+++ Server/server.js
@@ -1,35 +1,62 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
 
-const adminProductRouter = require('./routes/admin/products-routes')
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
+
+const commonFeatureRouter = require("./routes/common/feature-routes");
+
 
 mongoose
-.connect("mongodb+srv://123:123@cluster0.nw8ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {console.log('Connected to MongoDB')})
-.catch((err) => {console.log('Failed to connect to MongoDB', err)});
+  .connect("mongodb+srv://123:123@cluster0.nw8ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
-    cors({
-
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST", "PUT", "DELETE"], 
-        allowedHeaders: ["Content-Type", "Authorization" , "cache-control", "Expires", "Pragma", "Origin", "Accept", "Accept-Encoding", "Accept-Language", "Host", "Referer", "User-Agent"],
-        credentials: true
-    })
-
-
-)
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
 
-app.use('/api/admin/products', adminProductRouter);
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.use("/api/common/feature", commonFeatureRouter);
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+
+// To update all dependencies, use the following commands:
+// 1. Install npm-check-updates globally: npm install -g npm-check-updates
+// 2. Update the dependencies in package.json: ncu -u
+// 3. Install the updated dependencies: npm install
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import { Skeleton } from "@/components/ui/skeleton";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import AdminLayout from "./components/admin-view/layout";
-import AuthLayout from "./components/common/layout";
-import CheckAuth from "./components/common/check-auth";
-import ShoppingLayout from "./components/shopping-view/layout";
-// import AdminDashboard from "./pages/admin-view/dashboard";
-import AdminFeatures from "./pages/admin-view/features";
-import AdminOrders from "./pages/admin-view/orders";
-import AdminProducts from "./pages/admin-view/products";
+import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashbord";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
 import NotFound from "./pages/not-found";
-import ShoppingAccount from "./pages/shopping-view/account";
-import ShoppingCheckout from "./pages/shopping-view/checkout";
 import ShoppingHome from "./pages/shopping-view/home";
 import ShoppingListing from "./pages/shopping-view/listing";
-// import PaymentSuccessPage from "./pages/shopping-view/payment-success";
-// import PaypalReturnPage from "./pages/shopping-view/paypal-return";
-// import SearchProducts from "./pages/shopping-view/search";
-// import UnauthPage from "./pages/unauth-page";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingAccount from "./pages/shopping-view/account";
+import CheckAuth from "./components/common/check-auth";
+import UnauthPage from "./pages/unauth-page";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { checkAuth } from "./store/auth-slice";
+import { Skeleton } from "@/components/ui/skeleton";
+import PaypalReturnPage from "./pages/shopping-view/paypal-return";
+import PaymentSuccessPage from "./pages/shopping-view/payment-success";
+import SearchProducts from "./pages/shopping-view/search";
 
 function App() {
   const { user, isAuthenticated, isLoading } = useSelector(
@@ -68,7 +68,7 @@ function App() {
             </CheckAuth>
           }
         >
-          {/* <Route path="dashboard" element={<AdminDashboard />} /> */}
+          <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
@@ -76,20 +76,20 @@ function App() {
         <Route
           path="/shop"
           element={
-            // <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            // </CheckAuth>
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
               <ShoppingLayout />
+            </CheckAuth>
           }
         >
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
-          {/* <Route path="paypal-return" element={<PaypalReturnPage />} /> */}
-          {/* <Route path="payment-success" element={<PaymentSuccessPage />} /> */}
-          {/* <Route path="search" element={<SearchProducts />} /> */}
+          <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
-        {/* <Route path="/unauth-page" element={<UnauthPage />} /> */}
+        <Route path="/unauth-page" element={<UnauthPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
